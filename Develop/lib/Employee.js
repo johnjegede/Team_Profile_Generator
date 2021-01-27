abb8ba9c177fad1b619f27d4e6fdbb128216e158
John Jegede <jegede400@gmail.com>
2021-01-27 00:01:49 -0500

    added class Employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee{
+    constructor(name, Id , email){
+        this.Name = name;
+        this.Id = Id;
+        this.Email = email;
+    }
+    getName(){
+        return this.name;
+    }
+    
+    getId(){
+        return this.Id;
+    }
+
+    getEmail(){
+        return this.Email;
+    }
+
+    getRole(){
+        return "Employee";
+    }
+}

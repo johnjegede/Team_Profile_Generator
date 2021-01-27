3bb8c1d5e10f73225bf611c0c35db5690f90ebc1
John Jegede <jegede400@gmail.com>
2021-01-27 00:45:54 -0500

    added class for Intern

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee{
+    constructor(name, Id , email, officeNumber){
+        super(name, Id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    getRole(){
+        return "Engineer";
+    }
+
+}
